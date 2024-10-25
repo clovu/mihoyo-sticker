d77805350f54bd3079ea871c156e99ff23ebf366
Clover You <2621869236@qq.com>
2024-10-25 18:59:05 +0800

    chore: test lint-stage

--- src/lib/sticker.test.ts
+++ src/lib/sticker.test.ts
@@ -4,7 +4,7 @@ import { findDefaultStickerClassify } from "./sticker"
 describe('sticker utils', () => {
   test('find a sticker classify that can be the default', () => {
     const sticker = findDefaultStickerClassify(
-      [{ icon: "", id: 1 }, { icon: "https://xxx", id: 2 }, { icon: "https://xxx", id: 3 }] as any[]
+         [{ icon: "", id: 1 }, { icon: "https://xxx", id: 2 }, { icon: "https://xxx", id: 3 }] as any[]
     )
     expect(sticker?.id).toBe(2)
   })
