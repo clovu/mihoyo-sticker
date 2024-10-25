ec1b0251fe77b23df6e1c7a1023982c117a99115
Clover You <2621869236@qq.com>
2024-10-25 17:40:05 +0800

    feat: add development notice to website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons'
+import { ExclamationTriangleIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
 import { StickerCard } from '~/components/sticker-card'
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert'
 
 import { Button } from '~/components/ui/button'
 import { Input } from '~/components/ui/input'
@@ -7,6 +8,14 @@ import { Input } from '~/components/ui/input'
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Alert variant="destructive">
+        <ExclamationTriangleIcon className="h-4 w-4" />
+        <AlertTitle>Alert</AlertTitle>
+        <AlertDescription>
+          The current website is under urgent development.
+        </AlertDescription>
+      </Alert>
+
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <StickerCard className="w-[636px]" />
         <div className="flex gap-2 mx-auto">
