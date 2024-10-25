e12f0bb0f2a8e2d853ceb230496c37b0c4b75a78
Clover You <2621869236@qq.com>
2024-10-25 17:16:29 +0800

    chore: clean

--- src/components/sticker-card.tsx
+++ src/components/sticker-card.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { MouseEvent, useState } from 'react'
 
-import { Card, CardContent, CardFooter, CardHeader } from '~/components/ui/card'
+import { Card, CardContent, CardFooter } from '~/components/ui/card'
 import { ScrollArea, ScrollBar } from '~/components/ui/scroll-area'
 import { StickerClassifyBar } from '~/components/classify-bar'
 
