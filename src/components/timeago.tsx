a4c4141618e95db14f7f34da2bded4909919bb50
Clover You <2621869236@qq.com>
2025-02-24 11:41:26 +0800

    fix: time ago not work

--- src/components/timeago.tsx
+++ src/components/timeago.tsx
@@ -9,5 +9,12 @@ export default function TimeAgoComponent({
 }>) {
   // In JavaScript, Date conversion uses local timezone if the date string contains timezone information
   const timestamp = new Date(date).getTime()
-  return <TimeAgo date={timestamp} suppressHydrationWarning />
+
+  const formatter: TimeAgo.Formatter = (value, unit, suffix, epochMiliseconds, nextFormatter) => {
+    if (unit === 'second')
+      return 'now'
+    return nextFormatter?.(value, unit, suffix, epochMiliseconds)
+  }
+
+  return <TimeAgo date={timestamp} minPeriod={10} formatter={formatter} />
 }
